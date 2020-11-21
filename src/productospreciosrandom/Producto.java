8d63f0bf7001049585434bdec0718ac9bf3b0af9
estudioddai <estudiodortiz@gmail.com>
2020-11-21 11:14:43 -0300

    clase producto creacion

--- src/productospreciosrandom/Producto.java
+++ src/productospreciosrandom/Producto.java
@@ -1,6 +1,39 @@
 package productospreciosrandom;
 
-public class Producto {
+import java.text.DecimalFormat;
 
+public class Producto {
+	
+		private int codigo;
+		private double precio;
+		
+		public Producto(int codigo, double precio) {
+			this.codigo = codigo;
+			this.precio = precio;
+			
+		}
+
+		public int getCodigo() {
+			return codigo;
+		}
+
+		public void setCodigo(int codigo) {
+			this.codigo = codigo;
+		}
+
+		public double getPrecio() {
+			return precio;
+		}
+
+		public void setPrecio(double precio) {
+			this.precio = precio;
+		}
+		
+		public String toString() {
+			DecimalFormat decimalFormat = new DecimalFormat("#.##");
+			return String.format("Codigo: %s - Precio: $%s", codigo, decimalFormat.format(precio));
+			
+		}
+		
 	
 	}
